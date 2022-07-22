f51d1d6a7b1de9449635d894b1b74ca72e41c6ee
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2022-07-22 11:37:13 -0700

    Remove console.log from AuthContext

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -13,21 +13,21 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
   const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
+    return createUserWithEmailAndPassword( auth, email, password );
   };
 
    const signIn = (email, password) =>  {
-    return signInWithEmailAndPassword(auth, email, password)
+    return signInWithEmailAndPassword( auth, email, password )
    }
 
   const logout = () => {
-      return signOut(auth)
+      return signOut( auth )
   }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser);
-      setUser(currentUser);
+      // console.log( currentUser );
+      setUser( currentUser );
     });
     return () => {
       unsubscribe();
